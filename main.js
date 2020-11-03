79756f98cb50c9d66c3cf2d9fb213709df17de3f
Maksymilian Szabatin <szabatin.yt@gmail.com>
2020-11-03 20:33:30 +0100

    js emptyParagraph

--- main.js
+++ main.js
@@ -2,3 +2,7 @@ const firstName = "Maks";
 const age = "20";
 
 console.log(`Dzień dobry widzu, mam na imię ${firstName} i mam ${age} lat.`);
+
+const emptyParagraph = document.querySelector ('.page__title--js')
+
+emptyParagraph.innerHTML = 'Autorem wszystkich artykułów jest Maksymilian Szabatin'
